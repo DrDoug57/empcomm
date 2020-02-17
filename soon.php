3527a54090ab6c60d439fb5944936f9af99a7665
DrDoug57 <34461747+DrDoug57@users.noreply.github.com>
2020-02-17 15:14:46 -0800

    Add files via upload

---
+++ soon.php
@@ -0,0 +1,22 @@
+<table width="400" border="0" align="center" cellpadding="3" cellspacing="1">
+<tr>
+<td align="center"><strong> Coming Soon </strong></td>
+</tr>
+</table>
+
+<table width="400" border="0" align="center" cellpadding="0" cellspacing="1">
+<tr>
+<td>
+Please note: This function is currently not completed.<br>
+We appologize for any inconvienences this may cause.
+</td>
+</tr>
+</table>
+
+</td>
+</tr>
+</table>
+<br>
+<Center>
+ <A href="index.html" target="_top"><img align="absmiddle" src="images/nav-0-0-L.png"></A>
+ </Center>
